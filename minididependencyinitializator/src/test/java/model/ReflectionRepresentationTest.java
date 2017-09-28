4c12ed1df089ac9375ef0a70eb98ffaa059d9b35
Aki Väliaho <akivvaliaho@gmail.com>
2017-09-28 11:33:59 +0300

    cleaning mocks from tests

--- minididependencyinitializator/src/test/java/model/ReflectionRepresentationTest.java
+++ minididependencyinitializator/src/test/java/model/ReflectionRepresentationTest.java
@@ -7,9 +7,6 @@ import tooling.DummyTestClassWithFieldDependency;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ReflectionRepresentationTest {
     private ReflectionRepresentation reflectionRepresentation;
@@ -17,20 +14,18 @@ public class ReflectionRepresentationTest {
 
     @Before
     public void setUp() throws Exception {
-        reflectionTool = mock(ReflectionTool.class);
+        reflectionTool = new ReflectionTool();
         this.reflectionRepresentation = new ReflectionRepresentation(DummyTestClassWithDependency.class, reflectionTool);
     }
 
     @Test
     public void hasNoInjectedFields() throws Exception {
-        when(reflectionTool.hasAutowiredAnnotation(any())).thenReturn(false);
         assertTrue(!reflectionRepresentation.hasInjectedFields());
     }
 
     @Test
     public void hasInjectedFields() throws Exception {
         this.reflectionRepresentation = new ReflectionRepresentation(DummyTestClassWithFieldDependency.class, reflectionTool);
-        when(reflectionTool.hasAutowiredAnnotation(any())).thenReturn(true);
         assertTrue(reflectionRepresentation.hasInjectedFields());
     }
 
