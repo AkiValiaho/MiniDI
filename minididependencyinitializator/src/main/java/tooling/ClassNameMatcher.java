57111cd06caf303828a8e2e59139c5044aed13c8
Aki Väliaho <akivvaliaho@gmail.com>
2017-10-09 10:13:22 +0300

    Refactoring, added a test, deleted some unused code

--- minididependencyinitializator/src/main/java/tooling/ClassNameMatcher.java
+++ minididependencyinitializator/src/main/java/tooling/ClassNameMatcher.java
@@ -1,30 +1,23 @@
 package tooling;
 
-import java.lang.reflect.Field;
 import java.util.Iterator;
 import java.util.List;
 
 /**
  * Created by Aki on 29.9.2017.
  */
-public class ClassNameMatcher {
-    public boolean classNamesMatch(List<Object> objects, List<Class> classes) {
+class ClassNameMatcher {
+    boolean classNamesMatch(List<Object> objects, List<Class> classes) {
         if (objects.size() != classes.size()) {
             return false;
         }
         final Iterator<Object> objectIterator = objects.iterator();
-        final Iterator<Class> classIterator = classes.iterator();
-        while (classIterator.hasNext()) {
-            final Class next = classIterator.next();
+        for (Class next : classes) {
             final Object next1 = objectIterator.next();
             if (!next.getName().equals(next1.getClass().getName())) {
                 return false;
             }
         }
         return true;
-    }
-
-    public boolean classNamesMatch(Field matchedField, Object matchedInstance) {
-        return matchedField.getType().equals(matchedInstance.getClass());
     }
 }
---
+++ minididependencyinitializator/src/test/java/tooling/ClassNameMatcherTest.java
@@ -0,0 +1,40 @@
+package tooling;
+
+import model.dummyClasses.DummyTestClassB;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Aki on 9.10.2017.
+ */
+public class ClassNameMatcherTest {
+    private ClassNameMatcher classNameMatcher;
+
+    @Before
+    public void setUp() throws Exception {
+        this.classNameMatcher = new ClassNameMatcher();
+    }
+
+
+    @Test
+    public void classNamesMatch_namesDontMatch_shouldReturnFalse() throws Exception {
+        final List<Object> dummyTestClasses = Collections.singletonList(new DummyTestClass());
+        assertTrue(doesNotMatch(dummyTestClasses, Collections.singletonList(DummyTestClassB.class)));
+    }
+
+    private boolean doesNotMatch(List<Object> dummyTestClasses, List<Class> classes) {
+        return !this.classNameMatcher.classNamesMatch(dummyTestClasses, classes);
+    }
+
+    @Test
+    public void classNamesMatch_namesMatch_shouldReturnTrue() throws Exception {
+        final List<Object> dummyTestClasses = Collections.singletonList(new DummyTestClass());
+        assertTrue(this.classNameMatcher.classNamesMatch(dummyTestClasses, Collections.singletonList(DummyTestClass.class)));
+    }
+
+}
