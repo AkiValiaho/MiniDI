0b2e12ebe97362ac44e050d799cc513f552b1678
Aki Väliaho <akivvaliaho@gmail.com>
2017-10-12 19:17:59 +0300

    Added a test

--- minididependencyinitializator/src/test/java/model/DependencyReflectionRepresentationTest.java
+++ minididependencyinitializator/src/test/java/model/DependencyReflectionRepresentationTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import tooling.DummyTestClassWithDependency;
 import tooling.DummyTestClassWithFieldDependency;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import java.lang.reflect.Constructor;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 public class DependencyReflectionRepresentationTest {
     private DependencyReflectionRepresentation dependencyReflectionRepresentation;
@@ -34,19 +36,28 @@ public class DependencyReflectionRepresentationTest {
 
     @Test
     public void getDependentParamsFromFields() throws Exception {
-        //dependencyReflectionRepresentation.getDependentParamsFromFields();
+        this.dependencyReflectionRepresentation = new DependencyReflectionRepresentation(DummyTestClassWithFieldDependency.class);
+        assertEquals(dependencyReflectionRepresentation.getDependentParamsFromFields().length, 1);
     }
 
     @Test
     public void represent() throws Exception {
+        this.dependencyReflectionRepresentation = new DependencyReflectionRepresentation(DummyTestClassWithFieldDependency.class);
+        DependencyReflectionRepresentation represent = dependencyReflectionRepresentation.represent();
+        Class<?>[] dependentParamsFromFields = represent.getDependentParamsFromFields();
+        assertEquals(dependentParamsFromFields.length, 1);
     }
 
     @Test
     public void getParamTypesFromArgsConstructor() throws Exception {
+        Class<?>[] paramTypesFromArgsConstructor = dependencyReflectionRepresentation.getParamTypesFromArgsConstructor();
+        assertEquals(paramTypesFromArgsConstructor.length, 1);
     }
 
     @Test
     public void getArgsConstructor() throws Exception {
+        Optional<Constructor> argsConstructor = dependencyReflectionRepresentation.getArgsConstructor();
+        assertTrue(argsConstructor.isPresent());
     }
 
 }
